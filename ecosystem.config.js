1a122b3e3a33fcd69a04b14e6d22d141b847b41f
Rian Zulfariansyah <rianzulfariansyah@Rians-MacBook-Air.local>
2026-09-02 12:10:01 +0800

    fix: set interpreter none in ecosystem.config.js for PM2 Gunicorn binary

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,9 @@ module.exports = {
   apps: [
     {
       name: "absen-seminar",
-      script: "venv/bin/gunicorn",
+      script: "./venv/bin/gunicorn",
       args: "--workers 3 --bind 127.0.0.1:5000 wsgi:app",
-      cwd: "./",
-      instances: 1,
+      interpreter: "none",
       autorestart: true,
       watch: false,
       max_memory_restart: "300M",
